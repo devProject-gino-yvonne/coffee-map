35d752459604d99dc06116cb504138004a6785cd
Yvonne C <yvonneycheng@liontravel.com>
2025-07-06 15:33:18 +0800

    feat:新增會員登入zod驗證

--- src/app/[lang]/(member)/login/page.tsx
+++ src/app/[lang]/(member)/login/page.tsx
@@ -2,7 +2,7 @@ import LoginPage from '@/components/loginPage';
 export default function Login() {
   return (
     <div className="flex flex-col items-center justify-center p-20">
-      <h1 className="text-2xl font-bold pb-4">會員登入</h1>
+      <h1 className="text-2xl font-bold pb-4 text-[#5a3d1b]">會員登入</h1>
       <LoginPage />
     </div>
   );
