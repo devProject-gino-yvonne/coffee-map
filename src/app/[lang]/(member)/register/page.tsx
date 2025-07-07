7e7d2fe7d4181b6d979e5ec73b5801dcc769d1bd
ginohsu <ginohsu16@gmail.com>
2025-07-08 00:06:12 +0800

    refactor: remove src directory
    add prisma example in register page

--- src/app/[lang]/(member)/register/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Register() {
-  return (
-    <div className="flex items-center justify-center min-h-screen p-8">
-      <h1 className="text-2xl font-bold">Register Page</h1>
-    </div>
-  );
-}
